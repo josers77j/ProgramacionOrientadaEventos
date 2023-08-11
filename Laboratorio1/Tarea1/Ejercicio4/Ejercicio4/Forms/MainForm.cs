55345786ce873f1b426c3166e09622a0b7a9d341
josers77j <josers77.1@gmail.com>
2023-08-11 16:34:02 -0600

    Ejercicio 4 terminado

--- Laboratorio1/Tarea1/Ejercicio4/Ejercicio4/Forms/MainForm.cs
+++ Laboratorio1/Tarea1/Ejercicio4/Ejercicio4/Forms/MainForm.cs
@@ -21,23 +21,41 @@ namespace Ejercicio4.Forms
         {
             try
             {
+                //validacion del campo de texto
                 if (!string.IsNullOrEmpty(NumeroTextBox.Text) && 
                     int.TryParse(NumeroTextBox.Text, out int numero) &&
                     numero>0)
                 {
-                    for (int i = numero; i == 1; i--)
-                    {
+                    //es necesario definir las variables desde antes
+                    string recorrido = "";
+                    int sumatoria = 0;
 
-                    }                  
+                    for (int i = numero; i >= 1; i--)
+                    {   //concatenando la variable i mientras se hace el recorrido
+                        recorrido += $"{i}";
+                        sumatoria += i;
+
+                        // Agregar "+" si no es el último número
+                        if (i > 1)
+                        {
+                            recorrido += " + ";
+                        }
+                    }
+                    //Mostrar Mensajes
+                    MessageBox.Show($"{recorrido} = {sumatoria}", "Sumatoria");
+                    NumeroCapturadoLabel.Text = Convert.ToString(numero);
+                    ResultadoLabel.Text = Convert.ToString(sumatoria);
                 }
                 else
                 {
-                    MessageBox.Show("Letras, valores negativos o un valor cero, no son permitidos");
+                    //cualquier cosa que no cumpla las condiciones, aqui.
+                    MessageBox.Show("Unicamente valores de tipo enteros");
                     NumeroTextBox.Text = "";
                 }
             }catch
             (Exception ex)
             {
+                //si ocurre un error fuera de lo previsto, aqui.
                 MessageBox.Show($"Error : {ex.Message}");
             }
         }
